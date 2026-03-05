5df663e313ab437e3936ae161889a6f2b44f25a9
Marwan Hussein <marwan.hussein@pwc.com>
2026-03-05 13:36:01 +0200

    Implement the page object model

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
+import { PlaywrightDevPage } from '../pages/playwright-dev-page';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('getting started should contain table of contents', async ({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.goto();
+  await playwrightDev.getStarted();
+  await expect(playwrightDev.tocList).toHaveText([
+    `How to install Playwright`,
+    `What's installed`,
+    `How to run the example test`,
+    `How to open the HTML test report`,
+    `Write tests using web-first assertions, fixtures and locators`,
+    `Run single or multiple tests; headed mode`,
+    `Generate tests with Codegen`,
+    `View a trace of your tests`
+  ]);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
