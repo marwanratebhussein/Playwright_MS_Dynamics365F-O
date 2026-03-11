e602d2aa6c226aad610e85ec67af4b448255256c
Marwan Hussein <marwan.hussein@pwc.com>
2026-03-11 11:19:35 +0200

    Updates on the workflow to check F&O tests

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,4 +1,5 @@
 import { test as setup, expect } from '@playwright/test';
+import { TIMEOUT } from 'dns';
 import path from 'path';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
@@ -8,13 +9,18 @@ setup('authenticate', async ({ page }) => {
   await page.goto('https://de-cd-demo1.sandbox.operations.eu.dynamics.com/?cmp=usmf&mi=DefaultDashboard');
   await page.locator('xpath=//input[@type="email"]').fill('marwan.hussein@pwc.com');  
   await page.locator('xpath=//input[@type="submit"]').click();
+  await page.locator('xpath=//a[@id="idA_PWD_SwitchToCredPicker"]').click();
+  await page.locator('xpath=//div[@aria-label="Use my password"]').click();
+  await page.locator('xpath=//input[@type="password"]').fill('Messi_Pulga_Goat+_10');
+  await page.locator('xpath=//input[@type="submit"]').click();
+  await page.locator('xpath=//input[@type="submit"]').click();
   // Wait until the page receives the cookies.
   //
   // Sometimes login flow sets cookies in the process of several redirects.
   // Wait for the final URL to ensure that the cookies are actually set.
-  await page.waitForURL('https://de-cd-demo1.sandbox.operations.eu.dynamics.com/?cmp=usmf&mi=DefaultDashboard');
+  //await page.waitForURL('https://de-cd-demo1.sandbox.operations.eu.dynamics.com/?cmp=usmf&mi=DefaultDashboard');
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
-  await expect(page.locator('xpath=//span[@id="NavBarDashboard_label"]')).toBeVisible();
+  await expect(page.locator('xpath=//span[@id="NavBarDashboard_label"]')).toBeVisible({timeout:10000});
 
   // End of authentication steps.
 
